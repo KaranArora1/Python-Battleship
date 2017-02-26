e78bc7562684b35e665a81042dfa57051eebbe5d
KaranArora1 <karanarora@Karans-MacBook-Pro.local>
2017-02-26 13:34:38 -0500

    Finished P1 Fleet
    Added boxes, string lists, and box lists to player1’s board. Fleet is
    now done.

--- var.py
+++ var.py
@@ -73,6 +73,54 @@ F8_1=Rectangle(Point(381.5, 325), Point(431, 378))
 F9_1=Rectangle(Point(431, 325), Point(480.5, 378))
 F10_1=Rectangle(Point(480.5, 325), Point(530, 378))
 
+#G
+G1_1=Rectangle(Point(35, 378), Point(84.5, 431))
+G2_1=Rectangle(Point(84.5, 378), Point(134, 431))
+G3_1=Rectangle(Point(134, 378), Point(183.5, 431))
+G4_1=Rectangle(Point(183.5, 378), Point(233, 431))
+G5_1=Rectangle(Point(233, 378), Point(282.5, 431))
+G6_1=Rectangle(Point(282.5, 378), Point(332, 431))
+G7_1=Rectangle(Point(332, 378), Point(381.5, 431))
+G8_1=Rectangle(Point(381.5, 378), Point(431, 431))
+G9_1=Rectangle(Point(431, 378), Point(480.5, 431))
+G10_1=Rectangle(Point(480.5, 378), Point(530, 431))
+
+#H
+H1_1=Rectangle(Point(35, 431), Point(84.5, 484))
+H2_1=Rectangle(Point(84.5, 431), Point(134, 484))
+H3_1=Rectangle(Point(134, 431), Point(183.5, 484))
+H4_1=Rectangle(Point(183.5, 431), Point(233, 484))
+H5_1=Rectangle(Point(233, 431), Point(282.5, 484))
+H6_1=Rectangle(Point(282.5, 431), Point(332, 484))
+H7_1=Rectangle(Point(332, 431), Point(381.5, 484))
+H8_1=Rectangle(Point(381.5, 431), Point(431, 484))
+H9_1=Rectangle(Point(431, 431), Point(480.5, 484))
+H10_1=Rectangle(Point(480.5, 431), Point(530, 484))
+
+#I
+I1_1=Rectangle(Point(35, 484), Point(84.5, 537))
+I2_1=Rectangle(Point(84.5, 484), Point(134, 537))
+I3_1=Rectangle(Point(134, 484), Point(183.5, 537))
+I4_1=Rectangle(Point(183.5, 484), Point(233, 537))
+I5_1=Rectangle(Point(233, 484), Point(282.5, 537))
+I6_1=Rectangle(Point(282.5, 484), Point(332, 537))
+I7_1=Rectangle(Point(332, 484), Point(381.5, 537))
+I8_1=Rectangle(Point(381.5, 484), Point(431, 537))
+I9_1=Rectangle(Point(431, 484), Point(480.5, 537))
+I10_1=Rectangle(Point(480.5, 484), Point(530, 537))
+
+#J
+J1_1=Rectangle(Point(35, 537), Point(84.5, 590))
+J2_1=Rectangle(Point(84.5, 537), Point(134, 590))
+J3_1=Rectangle(Point(134, 537), Point(183.5, 590))
+J4_1=Rectangle(Point(183.5, 537), Point(233, 590))
+J5_1=Rectangle(Point(233, 537), Point(282.5, 590))
+J6_1=Rectangle(Point(282.5, 537), Point(332, 590))
+J7_1=Rectangle(Point(332, 537), Point(381.5, 590))
+J8_1=Rectangle(Point(381.5, 537), Point(431, 590))
+J9_1=Rectangle(Point(431, 537), Point(480.5, 590))
+J10_1=Rectangle(Point(480.5, 537), Point(530, 590))
+
 #LooperVars
 Astr=("A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10")
 Bstr=("B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10")
@@ -80,6 +128,10 @@ Cstr=("C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10")
 Dstr=("D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10")
 Estr=("E1", "E2", "E3", "E4", "E5", "E6", "E7", "E8", "E9", "E10")
 Fstr= ("F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10")
+Gstr= ("G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8", "G9", "G10")
+Hstr= ("H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10")
+Istr= ("I1", "I2", "I3", "I4", "I5", "I6", "I7", "I8", "I9", "I10")
+Jstr= ("J1", "J2", "J3", "J4", "J5", "J6", "J7", "J8", "J9", "J10")
 
 #BoxesP1
 BoxesP1A=(A1_1, A2_1, A3_1, A4_1, A5_1, A6_1, A7_1, A8_1, A9_1, A10_1)
@@ -88,6 +140,10 @@ BoxesP1C=(C1_1, C2_1, C3_1, C4_1, C5_1, C6_1, C7_1, C8_1, C9_1, C10_1)
 BoxesP1D=(D1_1, D2_1, D3_1, D4_1, D5_1, D6_1, D7_1, D8_1, D9_1, D10_1)
 BoxesP1E=(E1_1, E2_1, E3_1, E4_1, E5_1, E6_1, E7_1, E8_1, E9_1, E10_1)
 BoxesP1F=(F1_1, F2_1, F3_1, F4_1, F5_1, F6_1, F7_1, F8_1, F9_1, F10_1)
+BoxesP1G=(G1_1, G2_1, G3_1, G4_1, G5_1, G6_1, G7_1, G8_1, G9_1, G10_1)
+BoxesP1H=(H1_1, H2_1, H3_1, H4_1, H5_1, H6_1, H7_1, H8_1, H9_1, H10_1)
+BoxesP1I=(I1_1, I2_1, I3_1, I4_1, I5_1, I6_1, I7_1, I8_1, I9_1, I10_1)
+BoxesP1J=(J1_1, J2_1, J3_1, J4_1, J5_1, J6_1, J7_1, J8_1, J9_1, J10_1)
 
 #BoxesP2
 BoxesP2=[]
