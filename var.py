26e9713c0fbe7c786ce5e508d329e682831117e9
KaranArora1 <karanarora@Karans-MacBook-Pro.local>
2017-03-05 00:29:28 -0500

    Player2's Squares
    Added P2 squares

--- var.py
+++ var.py
@@ -121,6 +121,129 @@ J8_1=Rectangle(Point(381.5, 537), Point(431, 590))
 J9_1=Rectangle(Point(431, 537), Point(480.5, 590))
 J10_1=Rectangle(Point(480.5, 537), Point(530, 590))
 
+###############################################################################
+
+#Player2Board
+#A
+A1_2=Rectangle(Point(35, 60), Point(84.5, 113))
+A2_2=Rectangle(Point(84.5, 60 ), Point(134, 113))
+A3_2=Rectangle(Point(134, 60), Point(183.5, 113))
+A4_2=Rectangle(Point(183.5, 60), Point(233, 113))
+A5_2=Rectangle(Point(233, 60), Point(282.5, 113))
+A6_2=Rectangle(Point(282.5, 60), Point(332, 113))
+A7_2=Rectangle(Point(332, 60), Point(381.5, 113))
+A8_2=Rectangle(Point(381.5, 60), Point(431, 113))
+A9_2=Rectangle(Point(431, 60), Point(480.5, 113))
+A10_2=Rectangle(Point(480.5, 60), Point(530, 113))
+
+#B
+B1_2= Rectangle(Point(35, 113), Point(84.5, 166))
+B2_2= Rectangle(Point(84.5, 113), Point(134, 166))
+B3_2= Rectangle(Point(134, 113), Point(183.5, 166))
+B4_2= Rectangle(Point(183.5, 113), Point(233, 166))
+B5_2= Rectangle(Point(233, 113), Point(282.5, 166))
+B6_2= Rectangle(Point(282.5, 113), Point(332, 166))
+B7_2= Rectangle(Point(332, 113), Point(381.5, 166))
+B8_2= Rectangle(Point(381.5, 113), Point(431, 166))
+B9_2= Rectangle(Point(431, 113), Point(480.5, 166))
+B10_2= Rectangle(Point(480.5, 113), Point(530, 166))
+
+#C
+C1_2= Rectangle(Point(35, 166), Point(84.5, 219))
+C2_2=Rectangle(Point(84.5, 166), Point(134, 219))
+C3_2=Rectangle(Point(134, 166), Point(183.5, 219))
+C4_2=Rectangle(Point(183.5, 166), Point(233, 219))
+C5_2=Rectangle(Point(233, 166), Point(282.5, 219))
+C6_2=Rectangle(Point(282.5, 166), Point(332, 219))
+C7_2=Rectangle(Point(332, 166), Point(381.5, 219))
+C8_2=Rectangle(Point(381.5, 166), Point(431, 219))
+C9_2=Rectangle(Point(431, 166), Point(480.5, 219))
+C10_2=Rectangle(Point(480.5, 166), Point(530, 219))
+
+#D
+D1_2=Rectangle(Point(35, 219), Point(84.5, 272))
+D2_2=Rectangle(Point(84.5, 219), Point(134, 272))
+D3_2=Rectangle(Point(134, 219), Point(183.5, 272))
+D4_2=Rectangle(Point(183.5, 219), Point(233, 272))
+D5_2=Rectangle(Point(233, 219), Point(282.5, 272))
+D6_2=Rectangle(Point(282.5, 219), Point(332, 272))
+D7_2=Rectangle(Point(332, 219), Point(381.5, 272))
+D8_2=Rectangle(Point(381.5, 219), Point(431, 272))
+D9_2=Rectangle(Point(431, 219), Point(480.5, 272))
+D10_2=Rectangle(Point(480.5, 219), Point(530, 272))
+
+#E
+E1_2=Rectangle(Point(35, 272), Point(84.5, 325))
+E2_2=Rectangle(Point(84.5, 272), Point(134, 325))
+E3_2=Rectangle(Point(134, 272), Point(183.5, 325))
+E4_2=Rectangle(Point(183.5, 272), Point(233, 325))
+E5_2=Rectangle(Point(233, 272), Point(282.5, 325))
+E6_2=Rectangle(Point(282.5, 272), Point(332, 325))
+E7_2=Rectangle(Point(332, 272), Point(381.5, 325))
+E8_2=Rectangle(Point(381.5, 272), Point(431, 325))
+E9_2=Rectangle(Point(431, 272), Point(480.5, 325))
+E10_2=Rectangle(Point(480.5, 272), Point(530, 325))
+
+#F
+F1_2=Rectangle(Point(35, 325), Point(84.5, 378))
+F2_2=Rectangle(Point(84.5, 325), Point(134, 378))
+F3_2=Rectangle(Point(134, 325), Point(183.5, 378))
+F4_2=Rectangle(Point(183.5, 325), Point(233, 378))
+F5_2=Rectangle(Point(233, 325), Point(282.5, 378))
+F6_2=Rectangle(Point(282.5, 325), Point(332, 378))
+F7_2=Rectangle(Point(332, 325), Point(381.5, 378))
+F8_2=Rectangle(Point(381.5, 325), Point(431, 378))
+F9_2=Rectangle(Point(431, 325), Point(480.5, 378))
+F10_2=Rectangle(Point(480.5, 325), Point(530, 378))
+
+#G
+G1_2=Rectangle(Point(35, 378), Point(84.5, 431))
+G2_2=Rectangle(Point(84.5, 378), Point(134, 431))
+G3_2=Rectangle(Point(134, 378), Point(183.5, 431))
+G4_2=Rectangle(Point(183.5, 378), Point(233, 431))
+G5_2=Rectangle(Point(233, 378), Point(282.5, 431))
+G6_2=Rectangle(Point(282.5, 378), Point(332, 431))
+G7_2=Rectangle(Point(332, 378), Point(381.5, 431))
+G8_2=Rectangle(Point(381.5, 378), Point(431, 431))
+G9_2=Rectangle(Point(431, 378), Point(480.5, 431))
+G10_2=Rectangle(Point(480.5, 378), Point(530, 431))
+
+#H
+H1_2=Rectangle(Point(35, 431), Point(84.5, 484))
+H2_2=Rectangle(Point(84.5, 431), Point(134, 484))
+H3_2=Rectangle(Point(134, 431), Point(183.5, 484))
+H4_2=Rectangle(Point(183.5, 431), Point(233, 484))
+H5_2=Rectangle(Point(233, 431), Point(282.5, 484))
+H6_2=Rectangle(Point(282.5, 431), Point(332, 484))
+H7_2=Rectangle(Point(332, 431), Point(381.5, 484))
+H8_2=Rectangle(Point(381.5, 431), Point(431, 484))
+H9_2=Rectangle(Point(431, 431), Point(480.5, 484))
+H10_2=Rectangle(Point(480.5, 431), Point(530, 484))
+
+#I
+I1_2=Rectangle(Point(35, 484), Point(84.5, 537))
+I2_2=Rectangle(Point(84.5, 484), Point(134, 537))
+I3_2=Rectangle(Point(134, 484), Point(183.5, 537))
+I4_2=Rectangle(Point(183.5, 484), Point(233, 537))
+I5_2=Rectangle(Point(233, 484), Point(282.5, 537))
+I6_2=Rectangle(Point(282.5, 484), Point(332, 537))
+I7_2=Rectangle(Point(332, 484), Point(381.5, 537))
+I8_2=Rectangle(Point(381.5, 484), Point(431, 537))
+I9_2=Rectangle(Point(431, 484), Point(480.5, 537))
+I10_2=Rectangle(Point(480.5, 484), Point(530, 537))
+
+#J
+J1_2=Rectangle(Point(35, 537), Point(84.5, 590))
+J2_2=Rectangle(Point(84.5, 537), Point(134, 590))
+J3_2=Rectangle(Point(134, 537), Point(183.5, 590))
+J4_2=Rectangle(Point(183.5, 537), Point(233, 590))
+J5_2=Rectangle(Point(233, 537), Point(282.5, 590))
+J6_2=Rectangle(Point(282.5, 537), Point(332, 590))
+J7_2=Rectangle(Point(332, 537), Point(381.5, 590))
+J8_2=Rectangle(Point(381.5, 537), Point(431, 590))
+J9_2=Rectangle(Point(431, 537), Point(480.5, 590))
+J10_2=Rectangle(Point(480.5, 537), Point(530, 590))
+
 #LooperVars
 Astr=("A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10")
 Bstr=("B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10")
@@ -150,12 +273,21 @@ BoxesP2=[]
 
 #Player1Loc
 Player1_Locations=[]
+P1BoxLoc=[]
 
 #Player2Loc
 Player2_Locations=[]
+P1BoxConf=[]
 
 #P1conf
 P1confirmlist=[]
 
 #P2conf
 P2confirmlist=[]
+
+#Ships
+Bship1=[]
+Aircraft1=[]
+Pat1=[]
+Sub1=[]
+Frig1=[]
